a14dc174d765e7fa3b09a3cf1084996aa4ef3e32
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-09-09 17:03:54 -0300

    new route

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -8,4 +8,9 @@ app.url_map.strict_slashes = False
 @app.route('/')
 def home():
     return ("Hello HBNB!")
+
+@app.route('/hbnb')
+def hbnb():
+    return ("Hello HBNB!")
+
 app.run(host='0.0.0.0', port=5000)
