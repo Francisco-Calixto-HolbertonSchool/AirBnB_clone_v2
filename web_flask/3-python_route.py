be1b6a1dfe1e3202db2b601c87cea268c571d5e0
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-09-09 17:53:39 -0300

    is cool

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -23,9 +23,9 @@ def c_is_etc(text):
     """variable route"""
     return ("C {}".format(text.replace('_', ' ')))
 
-
-@app.route('/python/<text>')
-def python_is_etc(text="is cool"):
+@app.route('/python', defaults={'text': "is cool"})
+@app.route('/python/(<text>)')
+def python_is_etc(text):
     """variable route"""
     return ("Python {}".format(text.replace('_', ' ')))
 
