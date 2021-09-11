41f27453067b617c1f79ab196acf354dc760c7d3
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-09-11 17:45:28 -0300

    applied variable rules

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -30,10 +30,10 @@ def python_is_etc(text="is cool"):
     return ("Python {}".format(text.replace('_', ' ')))
 
 
-@app.route('/number/<n>', strict_slashes=False)
+@app.route('/number/<int:n>', strict_slashes=False)
 def reps_int(n):
     """variable route"""
-    return ("{} is a number".format(int(n)))
+    return ("{} is a number".format(n))
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
