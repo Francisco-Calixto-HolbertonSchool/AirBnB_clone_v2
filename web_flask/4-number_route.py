afecd96a1753d8cd11db4d42f9bbdb734d11b139
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-09-11 17:41:09 -0300

    a

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -33,7 +33,7 @@ def python_is_etc(text="is cool"):
 @app.route('/number/<n>', strict_slashes=False)
 def reps_int(n):
     """variable route"""
-    return ("{} is a number".format(n))
+    return ("{} is a number".format(int(n)))
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
