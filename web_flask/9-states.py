b732b98c7c60ac87bb35f4f2a9ce0853eb2a4fa2
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-09-12 17:27:45 -0300

    task 10

---
+++ web_flask/9-states.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""starts flask web app"""
+
+from flask import Flask
+from flask import render_template
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def remove_sesh(self):
+    '''remove the current SQLAlchemy Session after request'''
+    storage.close()
+
+
+@app.route('/states', strict_slashes=False)
+def states_list():
+    '''render html page with a list of states'''
+    objs = storage.all(State)
+    return (render_template('7-states_list.html', navigation=objs))
+
+
+@app.route('/states/<id>', strict_slashes=False)
+def states_by_id(id=None):
+    '''render html page with a list of states and its cities'''
+    objs = storage.all(State)
+    for state in objs.values():
+        if state.id == id:
+            name = state.name
+    return (render_template('9-states.html', state=name, id=id))
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
